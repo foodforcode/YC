c3a3ed60a417d8525184c9d24eeae0937b6640da
Connie <connieatsai@gmail.com>
2019-12-18 03:48:40 +0000

    initial routings

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-	res.send("landing page");
+	res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res){
+	var campgrounds = [
+		{name: "salmon creek", image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI0MX0&auto=format&fit=crop&w=700&q=80"},
+		{name: "Yosemite", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"},
+		{name: "goat hill", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"}
+	];
+	res.render("campgrounds", {campgrounds: campgrounds});
+	//{variable: property}
+});
+
+app.post("/campgrounds", function(req, res){
+	res.send("post campgrounds");
+	//get data from form and add to array
+	//redirect back to page (refresh)
+	
+});
 
 
 app.listen(3000, function(){
 	console.log("yelpin!!!");
 });
-
-git remote add origin https://github.com/foodforcode/YC.git
-git push -u origin master
