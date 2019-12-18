8f4641c638e1cdeabdb33be84ad09f5d0569b3a8
Connie <connieatsai@gmail.com>
2019-12-18 04:06:04 +0000

    form creation and redirect

--- app.js
+++ app.js
@@ -10,23 +10,31 @@ app.get("/", function(req, res){
 	res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
-	var campgrounds = [
+var campgrounds = [
 		{name: "salmon creek", image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI0MX0&auto=format&fit=crop&w=700&q=80"},
 		{name: "Yosemite", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"},
 		{name: "goat hill", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"}
 	];
+
+app.get("/campgrounds", function(req, res){
 	res.render("campgrounds", {campgrounds: campgrounds});
 	//{variable: property}
 });
 
-app.post("/campgrounds", function(req, res){
-	res.send("post campgrounds");
-	//get data from form and add to array
-	//redirect back to page (refresh)
-	
+app.get("/campgrounds/new", function(req, res){
+	res.render("new");
 });
 
+app.post("/campgrounds", function(req, res){
+	//get data from form and add to array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	//redirect back to page (refresh)
+	res.redirect("/campgrounds");
+	
+});
 
 app.listen(3000, function(){
 	console.log("yelpin!!!");
