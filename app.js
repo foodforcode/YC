a6e8a6b358ced7bd669a0bd476ce4d1cf943cf61
Connie <connieatsai@gmail.com>
2019-12-24 21:32:21 +0000

    install and require mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
